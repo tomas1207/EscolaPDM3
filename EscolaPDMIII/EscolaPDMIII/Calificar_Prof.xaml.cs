9f22896a1842aaaad5f8a115c132d1d4711a72a6
Pablo <pablo_espa@hotmail,es>
2019-02-24 01:29:22 +0000

    Final

--- EscolaPDMIII/EscolaPDMIII/Calificar_Prof.xaml.cs
+++ EscolaPDMIII/EscolaPDMIII/Calificar_Prof.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using EscolaPDMIII.MyDataSource;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,19 @@ namespace EscolaPDMIII
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Calificar_Prof : ContentPage
 	{
+        AlunoView alunoView = new AlunoView ();
 		public Calificar_Prof ()
 		{
 			InitializeComponent ();
-		}
-	}
+            alunoView = new AlunoView ();
+            FindByName("Aluno");
+            AlunoPicker.ItemsSource = alunoView.Alunos;
+        }
+
+
+        private void Aluno_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //AlunoPicker.Items[AlunoPicker.SelectedIndex]
+        }
+    }
 }
---
+++ EscolaPDMIII/EscolaPDMIII/MyDataSource/AlunosPicker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EscolaPDMIII.MyDataSource
+{
+    public class Aluno
+    {
+        public ArrayList Alunos { get; set; }
+
+
+
+        public List<Aluno> GetAluno()
+        {
+            List<Aluno> alunos = new List<Aluno>()
+            {
+                new Aluno () {Alunos = Core.GlogalVar.nome} 
+            };
+            return alunos;
+        }
+
+
+    };
+
+    public class AlunoView
+    {
+        public List<Aluno> Alunos { get; set; }
+        public AlunoView() //constructor
+        {
+            Alunos = new Aluno().GetAluno();
+        }
+    }
+}
